a74c53924aac2c77c4b116fff87b5f097b259f61
TimMaas002 <tim.maas@live.nl>
2021-02-16 15:49:11 +0100

    first commit

---
+++ src/main/java/nl/novi/maas/oliverheldens/demodrop/OHdemodropApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.maas.oliverheldens.demodrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OHdemodropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OHdemodropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/maas/oliverheldens/demodrop/OHdemodropApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.maas.oliverheldens.demodrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OHdemodropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
